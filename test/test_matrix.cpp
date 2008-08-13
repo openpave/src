6c3fec19d8b73269b662cda19307ea08d5fbd4b8
Jeremy D. Lea <reg@openpave.org>
2008-08-13 06:20:39 +0000

    Add some trial code for doing raw tensor math...  Maybe of use to Boris.

--- test/test_matrix.cpp
+++ test/test_matrix.cpp
@@ -324,3 +324,284 @@ main()
 	return 0;
 }
 #endif
+
+#undef N
+#undef W 
+
+#ifdef NOBUILD
+
+/*
+ * struct tensor_indexX - tensor indices
+ *
+ * We use a struct here rather than a raw char, because these create
+ * distinct types, so the compiler sees the various functions as distinct
+ * function signatures.  The 2, 3 and 4 valued ones are just for convience.
+ */
+template<char I>
+struct tensor_index { explicit tensor_index() {} };
+template<char I, char J>
+struct tensor_index2 { explicit tensor_index2() {} };
+template<char I, char J, char K>
+struct tensor_index3 { explicit tensor_index3() {} };
+template<char I, char J, char K, char L>
+struct tensor_index4 { explicit tensor_index4() {} };
+
+/*
+ * struct tensor2_expr - an encapsulated expression.
+ *
+ * This struct holds the result of an expression.  struct E is the result,
+ * and then we have two dimensions and two implicit indices.  The struct E
+ * is copied in the constructor, so that temporaries are not removed by
+ * the compiler.
+ */
+template<class E, unsigned M, unsigned N, char I, char J>
+struct tensor2_expr {
+	inline explicit tensor2_expr(const E & e) : m_e(e) {}
+	inline double operator() (const unsigned i, const unsigned j) const {
+		return m_e(i,j);
+	}
+private:
+	const E m_e;
+};
+
+// Forward declare
+template<unsigned M, unsigned N> struct tensor2;
+
+/*
+ * struct tensor2_ref - an encapsulated tensor2.
+ *
+ * This struct holds a reference to a tensor2, but adds the two implict
+ * indices, so that it can be used in expressions.
+ */
+template<unsigned M, unsigned N, char I, char J>
+struct tensor2_ref {
+	inline explicit tensor2_ref(tensor2<M,N> & e) : m_e(e) {}
+	inline double & operator() (const unsigned i, const unsigned j) {
+		return m_e(i,j);
+	}
+	inline double operator() (const unsigned i, const unsigned j) const {
+		return m_e(i,j);
+	}
+	tensor2_ref & operator= (const tensor2_ref & d) {
+		for (unsigned i = 0; i < M; i++)
+			for (unsigned j = 0; j < N; j++)
+				m_e(i+1,j+1) = d(i+1,j+1);
+		return *this;
+	}
+	template<class E1>
+	tensor2_ref & operator= (const tensor2_expr<E1,M,N,I,J> & d) {
+		for (unsigned i = 0; i < M; i++)
+			for (unsigned j = 0; j < N; j++)
+				m_e(i+1,j+1) = d(i+1,j+1);
+		return *this;
+	}
+private:
+	tensor2<M,N> & m_e;
+};
+
+/*
+ * struct tensor2 - a second order tensor of size M by N.
+ *
+ * This is the real tensor.
+ */
+template<unsigned M, unsigned N>
+struct tensor2 {
+	explicit tensor2() {
+	}
+	explicit tensor2(double d) {
+		for (unsigned i = 0; i < M; i++)
+			for (unsigned j = 0; j < N; j++)
+				data[i][j] = d;
+	}
+	explicit tensor2(const tensor2 & d) {
+		for (unsigned i = 0; i < M; i++)
+			for (unsigned j = 0; j < N; j++)
+				data[i][j] = d.data[i][j];
+	}
+	template<char I, char J>
+	const tensor2_expr<tensor2<M,N>,M,N,I,J> operator() (
+			tensor_index2<I,J>) const {
+		return tensor2_expr<tensor2<M,N>,M,N,I,J>(*this);
+	}
+	template<char I, char J>
+	tensor2_ref<M,N,I,J> operator() (tensor_index2<I,J>) {
+		return tensor2_ref<M,N,I,J>(*this);
+	}
+	inline double & operator() (const unsigned i, const unsigned j) {
+		return data[i-1][j-1];
+	}
+	inline double operator() (const unsigned i, const unsigned j) const {
+		return data[i-1][j-1];
+	}
+private:
+	double data[M][N];
+};
+
+/*
+ * struct tensor4_expr - an encapsulated expression.
+ *
+ * This struct holds the result of an expression.  struct E is the result,
+ * and then we have four dimensions and four implicit indices.  The struct E
+ * is copied in the constructor, so that temporaries are not removed by
+ * the compiler.
+ */
+template<class E, unsigned M, unsigned N, unsigned P, unsigned Q,
+		char I, char J, char K, char L>
+struct tensor4_expr {
+	inline explicit tensor4_expr(const E & e) : m_e(e) {}
+	inline double operator() (const unsigned i, const unsigned j,
+			const unsigned k, const unsigned l) const {
+		return m_e(i,j,k,l);
+	}
+private:
+	const E m_e;
+};
+
+// Forward declare
+template<unsigned M, unsigned N, unsigned P, unsigned Q> struct tensor4;
+
+/*
+ * struct tensor4_ref - an encapsulated tensor4.
+ *
+ * This struct holds a reference to a tensor4, but adds the four implict
+ * indices, so that it can be used in expressions.
+ */
+template<unsigned M, unsigned N, unsigned P, unsigned Q,
+		char I, char J, char K, char L>
+struct tensor4_ref {
+	inline explicit tensor4_ref(tensor4<M,N,P,Q> & e) : m_e(e) {}
+	inline double & operator() (const unsigned i, const unsigned j,
+			const unsigned k, const unsigned l) {
+		return m_e(i,j,k,l);
+	}
+	inline double operator() (const unsigned i, const unsigned j,
+			const unsigned k, const unsigned l) const {
+		return m_e(i,j,k,l);
+	}
+private:
+	tensor4<M,N,P,Q> & m_e;
+};
+
+/*
+ * struct tensor4_trans_XXXX - support for general fourth order transpose
+ */
+template<unsigned M, unsigned N, unsigned P, unsigned Q,
+		char I, char J, char K, char L>
+struct tensor4_trans_ikjl {
+	explicit tensor4_trans_ikjl(const tensor4<M,P,N,Q> & e) : m_e(e) { }
+	inline double operator() (const unsigned i, const unsigned j,
+			const unsigned k, const unsigned l) const {
+  		return m_e(i,k,j,l);
+  	}
+private:
+	const tensor4<M,P,N,Q> & m_e;
+};
+
+template<unsigned M, unsigned N, unsigned P, unsigned Q>
+struct tensor4 {
+public:
+	explicit tensor4() {
+	}
+	explicit tensor4(double d) {
+		for (unsigned i = 0; i < M; i++)
+			for (unsigned j = 0; j < N; j++)
+				for (unsigned k = 0; k < P; k++)
+					for (unsigned l = 0; l < Q; l++)
+						data[i][j][k][l] = d;
+	}
+	template<char I, char J, char K, char L>
+	tensor4_ref<M,N,P,Q,I,J,K,L>
+	operator() (tensor_index4<I,J,K,L>) {
+		return tensor4_ref<M,N,P,Q,I,J,K,L>(*this);
+	}
+	template<char I, char J, char K, char L>
+	const tensor4_expr<tensor4_trans_ikjl<M,P,N,Q,I,K,J,L>,M,P,N,Q,I,K,J,L>
+	operator() (tensor_index4<I,J,K,L>, tensor_index4<I,K,J,L>) {
+		return tensor4_expr<tensor4_trans_ikjl<M,P,N,Q,I,K,J,L>,
+				M,P,N,Q,I,K,J,L>(tensor4_trans_ikjl<M,P,N,Q,I,K,J,L>(*this));
+	}
+	inline double & operator() (unsigned i, unsigned j, unsigned k, unsigned l) {
+		return data[i-1][j-1][k-1][l-1];
+	}
+	inline double operator() (unsigned i, unsigned j, unsigned k, unsigned l) const {
+		return data[i-1][j-1][k-1][l-1];
+	}
+private:
+	double data[M][N][P][Q];
+};
+
+template<class E1, class E2, unsigned P, unsigned Q>
+struct tensor_expr_prod_ijkl_kl {
+	explicit tensor_expr_prod_ijkl_kl(const E1 & e1, const E2 & e2)
+	  : m_e1(e1), m_e2(e2) {}
+	inline double operator() (const unsigned i, const unsigned j) const {
+		double m = 0.0;
+		for (unsigned k = 0; k < P; k++)
+			for (unsigned l = 0; k < Q; k++)
+				m += m_e1(i,j,k,l)*m_e2(k,l);
+		return m;
+	}
+private:
+	const E1 m_e1;
+	const E2 m_e2;
+};
+template<unsigned M, unsigned N, unsigned P, unsigned Q,
+		char I, char J, char K, char L>
+inline tensor2_expr<tensor_expr_prod_ijkl_kl<
+	tensor4_ref<M,N,P,Q,I,J,K,L>,
+	tensor2_ref<P,Q,K,L>,P,Q>,M,N,I,J>
+operator* (const tensor4_ref<M,N,P,Q,I,J,K,L> & e1,
+		const tensor2_ref<P,Q,K,L> & e2) {
+	typedef tensor_expr_prod_ijkl_kl<
+		tensor4_ref<M,N,P,Q,I,J,K,L>,
+		tensor2_ref<P,Q,K,L>,P,Q> expr_t;
+	return tensor2_expr<expr_t,M,N,I,J>(expr_t(e1,e2));
+}
+template<class E, unsigned M, unsigned N, unsigned P, unsigned Q,
+		char I, char J, char K, char L>
+inline tensor2_expr<tensor_expr_prod_ijkl_kl<
+	tensor4_expr<E,M,N,P,Q,I,J,K,L>,
+	tensor2_ref<P,Q,K,L>,P,Q>,M,N,I,J>
+operator* (const tensor4_expr<E,M,N,P,Q,I,J,K,L> & e1,
+		const tensor2_ref<P,Q,K,L> & e2) {
+	typedef tensor_expr_prod_ijkl_kl<
+		tensor4_expr<E,M,N,P,Q,I,J,K,L>,
+		tensor2_ref<P,Q,K,L>,P,Q> expr_t;
+	return tensor2_expr<expr_t,M,N,I,J>(expr_t(e1,e2));
+}
+template<class E, unsigned M, unsigned N, unsigned P, unsigned Q,
+		char I, char J, char K, char L>
+inline tensor2_expr<tensor_expr_prod_ijkl_kl<
+	tensor4_ref<M,N,P,Q,I,J,K,L>,
+	tensor2_expr<E,P,Q,K,L>,P,Q>,M,N,I,J>
+operator* (const tensor4_ref<M,N,P,Q,I,J,K,L> & e1,
+		const tensor2_expr<E,P,Q,K,L> & e2) {
+	typedef tensor_expr_prod_ijkl_kl<
+		tensor4_ref<M,N,P,Q,I,J,K,L>,
+		tensor2_expr<E,P,Q,K,L>,P,Q> expr_t;
+	return tensor2_expr<expr_t,M,N,I,J>(expr_t(e1,e2));
+}
+
+int main()
+{
+	tensor_index2<'i','j'> ij;
+	tensor_index2<'k','l'> kl;
+	tensor_index2<'i','k'> ik;
+	tensor_index2<'j','l'> jl;
+	tensor_index4<'i','j','k','l'> ijkl;
+	tensor_index4<'i','k','j','l'> ikjl;
+	tensor_index2<'p','q'> pq;
+	const tensor2<3,3> e;
+	tensor2<3,3> b;
+	tensor2<3,3> s;
+	tensor2<4,4> a;
+	tensor4<3,3,3,3> E;
+	tensor4<4,3,4,3> K;
+
+	s(ij) = E(ijkl)*e(kl);
+	tensor2<3,3> w(s);
+	//w(pq) = E(ijkl)*e(kl); // Compile error...	
+	a(ik) = K(ijkl,ikjl)*b(jl);
+}
+
+#endif
